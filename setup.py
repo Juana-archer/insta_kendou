bdac799a764e222faea949651517c22cb89fc43d
Juana-archer <faniry1d@gmail.com>
2026-04-08 14:00:18 +0300

    Fix setup.py to include all modules

--- setup.py
+++ setup.py
@@ -3,12 +3,23 @@ from setuptools import setup, find_packages
 setup(
     name="insta_kendou",
     version="1.0.0",
-    author="juana_archer",
+    author="Juana-archer",
     description="Instagram automation tool",
     packages=find_packages(),
+    py_modules=[
+        "__init__",
+        "client",
+        "client1", 
+        "client2",
+        "client_bonne"
+    ],
+    include_package_data=True,
     install_requires=[
         "requests",
         "beautifulsoup4",
+        "selenium",
+        "cloudscraper",
+        "Telethon",
     ],
     python_requires=">=3.6",
 )
